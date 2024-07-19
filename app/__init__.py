c295a4e293782c1b9047f03b4a6c105aefcf8e6c
CodeWithMayank-Py <paliwalm4321@gmail.com>
2024-07-19 21:45:24 +0530

    Making changes in __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,6 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import logging
-from logging.handlers import RotatingFileHandler
-import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,14 +12,11 @@ migrate = Migrate(app, db)
 
 from app import routes, models
 
+# Setup console logging
 if not app.debug:
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/flask_app.log', maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    app.logger.addHandler(stream_handler)
 
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Flask App startup')
+app.logger.setLevel(logging.INFO)
+app.logger.info('Flask App startup')
